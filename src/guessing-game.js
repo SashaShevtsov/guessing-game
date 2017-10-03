2c16becbbcbb9e761f5610b8e29eb35f384fa114
Alex <Shavo4kin@gmail.com>
2017-10-03 15:35:37 +0300

    guessing-game: first update

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.floor((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = (this.min + this.max) / 2;
     }
 
     greater() {
-
+        this.min = (this.min + this.max) / 2;
     }
 }
 
